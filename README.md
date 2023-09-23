1759a3385fed2684bf5fc808dbeb6896bf2e3c72
ydl898898 <52091896+ydl898898@users.noreply.github.com>
2023-09-23 18:11:38 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# UUID
+```bash
+2b8aa0b8-79fb-4d11-ae41-3aa2f5272a9f
+```
---
+++ files/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+#定义哪吒变量参数
+export NEZHA_SERVER="nz.f4i.cn:5555"
+export NEZHA_KEY="5ddVS93Eq0Uc9he730"
+
+
+chmod +x server start.sh
+nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &
+
+tail -f /dev/null
